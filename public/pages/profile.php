cbccf5b1862125074c4a33b6f0f1d821bd526499
David Spencer <david.spencer@kcl.ac.uk>
2019-11-13 11:42:47 +0000

    remove HTML tags from profile page

--- public/pages/profile.php
+++ public/pages/profile.php
@@ -3,18 +3,6 @@
 <?php $page_title = 'Profile'; ?>
 <?php include(SHARED_PATH . '/header.php'); ?>
 
-<!doctype html>
-
-<html lang="en">
-  <head>
-    <title></title>
-    <meta charset="utf-8">
-  </head>
-
-  <body>
-    hello, this is the profile page.
-    
-  </body>
-</html>
+<?php echo "hello, this is the profile page." ?>
 
 <?php include(SHARED_PATH . '/footer.php'); ?>
