d3e330011afcccde1d1f9bea873c7d42f2a5dc60
David Spencer <david.spencer@kcl.ac.uk>
2019-11-13 11:16:14 +0000

    add generic database connection functions

---
+++ private/database.php
@@ -0,0 +1,37 @@
+<?php 
+
+    require_once('db_credentials.php');
+    
+    function db_connect() {
+        $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+        confirm_db_connect();
+        return $connection;
+    }
+    
+    function db_disconnect($connection) {
+        if(isset($connection)) {
+            mysqli_close($connection);
+        }
+    }
+    
+    function db_escape($connection, $input) {
+        return mysqli_real_escape_string($connection, $input);
+    }
+    
+    function confirm_db_connect() {
+        if(mysqli_connect_errno()) {
+            $messsage = "Failed database connection: ";
+            $messsage .= mysqli_connect_error();
+            $messsage .= " (" . mysqli_connect_errno() . ")";
+            exit($message);
+        }
+    }
+    
+    function confirm_result_set($result_set) {
+        if (!$result_set) {
+            exit("Database query failed!");
+        }
+    }
+    
+
+?>
