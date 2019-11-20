f9a4525c26a4084d5adfc61743ed2d186c0fb5d4
k1889687 <xaohan.jiang_chen@kcl.ac.uk>
2019-11-20 09:23:53 +0000

    Revert "Created header and footer"
    This reverts commit fee2fee7ab7cc359736da22906acbed8a254c888.

--- public/pages/index.php
+++ public/pages/index.php
@@ -1,1 +1,55 @@
+<html>
+    <head>
+        <style>
+            header{
+                box-sizing: border-box;
+                height:20%;
+            }
+            .column{
+                float: left;
+                width: 49%;
+                padding: 1px;
+            }
 
+            .logocolumn{
+                float: left;
+                width: 40%;
+                padding: 5px;
+            }
+            .row::after {
+                content: "";
+                clear: both;
+                display: table;
+            }
+        </style>
+        <title> KCL Chess Society </title>
+    </head>
+
+    <body>
+        <header>
+            <div class="row">
+                <div class="column">
+
+                    <div class="logocolumn">
+                        <img src="img_kcl.png" alt="KCL" style="width:100%;height:100%">
+                    </div>
+                    <div class="logocolumn">
+                        <img src="img_chess.jpg" alt="chess" style="width:100%;height:100%">
+                    </div>
+
+                </div>
+
+                <div class="column" style="font-size:20px">
+                    <br>
+                    <h1> KCL Chess Society </h1>
+                </div>
+            </div>
+        </header>
+
+        <h1>Welcome to the KCL Chess Club</h1>
+
+        <h1> KCL Chess Society </h1>
+    </body>
+
+    
+</html>
