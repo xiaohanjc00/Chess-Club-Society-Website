5052ae116d2a190d84c60f3465be4d7b6362efaf
David Spencer <david.spencer@kcl.ac.uk>
2019-11-20 17:15:36 +0000

    refactor profile.php to use database query and add edit_profile.php

--- public/pages/profile.php
+++ public/pages/profile.php
@@ -1,37 +1,40 @@
-<!-- <?php require_once('../../private/initialise.php'); ?> -->
-
-<?php echo "hello, this is the profile page." ?></br>
-
-<?php 
-    $first_name = "Firstname"; // replace with database query
-    $last_name = "Lastname"; // replace with database query
-    $name = $first_name . " " . $last_name;
-    $pic_url = "https://cdn3.f-cdn.com/contestentries/1376995/30494909/5b566bc71d308_thumb900.jpg"; // replace with database query
-    $profile_pic = "<a><img src=\"" . $pic_url . "\" alt=\"profile picture\" width=110></a>";
+<?php
+  require_once('../../private/initialise.php');
+  // require_login();
+  $id = $_GET['id'] ?? '1';
+  $user = find_user_by_id($id);
+  $first_name = h($user['first_name']);
+  $last_name = h($user['last_name']);
+  $picture = "<a><img src=\"" . h($user['picture']) . "\" alt=\"profile picture\" width=110></a>";
+  $rating = h($user['rating']);
+  $dob = h($user['dob']);
+  $gender = h($user['gender']);
+  $phone = h($user['phone']);
+  $address = h($user['address']);
+  $email = h($user['email']);
 ?>
 
 <?php $page_title = 'Profile'; ?>
-<!-- <?php include(SHARED_PATH . '/header.php'); ?> -->
-
-<?php echo "hello, this is the profile page." ?></br>
-<?php echo "soon you will be able to edit your name, address, phone number, gender and date of birth" ?></br>
-<?php echo "soon you will be able to withdraw as a member of the Chess Society and have all your data removed." ?></br>
+<?php include(SHARED_PATH . '/header.php'); ?>
 
 <div>
-    <h4>Full Name: </h4><?php echo $name ?>
-    <h4>Profile picture: </h4><?php echo $profile_pic ?>
-    <h4>Chess rating: </h4><?php echo "XXX" ?>
-    <h4>Email: </h4><?php echo "name@domain.com" ?>
-    <h4>Phone number: </h4><?php echo "12345678900" ?>
-    <h4>Mailing address: </h4><?php echo "123 Town, City, ZIP" ?>
-    <h4>Gender: </h4><?php echo "F / M / -" ?>
-    <h4>Date of birth: </h4><?php echo "15/08/1997" ?>
-</div>
-<div>
+  <div>
+      <dl><dt>First name: </dt><dd><?php echo $first_name ?></dd></dl>
+      <dl><dt>Last name: </dt><dd><?php echo $last_name ?></dd></dl>
+      <dl><dt>Profile picture: </dt><dd><?php echo $picture ?></dd></dl>
+      <dl><dt>Chess rating: </dt><dd><?php echo $rating ?></dd></dl>
+      <dl><dt>Date of birth: </dt><dd><?php echo $dob ?></dd></dl>
+      <dl><dt>Gender: </dt><dd><?php echo $gender ?></dd></dl>
+      <dl><dt>Phone number: </dt><dd><?php echo $phone ?></dd></dl>
+      <dl><dt>Mailing address: </dt><dd><?php echo $address ?></dd></dl>
+      <dl><dt>Email: </dt><dd><?php echo $email ?></dd></dl>
+  </div>
+  <div>
     <p>
-        <a href="url_for_edit_profile_details_page">Edit Profile</a></br>
-        <a href="url_for_delete_confirmation_page">Cancel membership</a></br>
+      <a href="<?php echo url_for('/edit_profile.php?id=' . h(u($user['id']))); ?>">Edit Profile</a></br>
+      <a href="<?php echo url_for('/delete_profile.php?id=' . h(u($admin['id']))); ?>">Cancel membership</a></br>
     </p>
+  </div>
 </div>
 
-<!-- <?php include(SHARED_PATH . '/footer.php'); ?> -->
+<?php include(SHARED_PATH . '/footer.php'); ?>
