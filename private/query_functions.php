77c3dd2da88e95a6c5e370e121243f4c0c7630fc
David Spencer <david.spencer@kcl.ac.uk>
2019-11-20 14:53:36 +0000

    refactor index.php (homepage) and add function headers for user updates

---
+++ private/query_functions.php
@@ -0,0 +1,17 @@
+<?php 
+
+function find_all_users() {}
+
+function find_user_by_id($id) {}
+
+function find_user_by_username($username) {}
+
+function validate_user($user, $options=[]) {}
+
+function insert_user($user) {}
+
+function update_user($admin) {}
+
+function delete_user($admin) {}
+    
+?>
