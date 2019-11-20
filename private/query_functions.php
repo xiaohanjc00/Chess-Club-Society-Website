bf0e447495abcaf769f87d5a0a7c99054145f6a9
David Spencer <david.spencer@kcl.ac.uk>
2019-11-20 15:39:08 +0000

    implement update_user function

--- private/query_functions.php
+++ private/query_functions.php
@@ -10,8 +10,39 @@ function validate_user($user, $options=[]) {}
 
 function insert_user($user) {}
 
-function update_user($admin) {}
+function update_user($user) {
+    global $db;
+    $password_sent = !is_blank($admin['password']);
+    $errors = validate_user($user, ['password_required' => $password_sent]);
+    if (!empty($errors)) {
+        return $errors;
+    }
+    $hashed_password = password_hash($user['password'], PASSWORD_DEFAULT);
+    $sql = "UPDATE users SET ";
+    $sql .= "first_name='" . db_escape($db, $user['first_name']) . "', ";
+    $sql .= "last_name='" . db_escape($db, $user['last_name']) . "', ";
+    
+    // TODO: add user fields on profile page
+    // TODO: implement all functions being used here
+    
+    
+    $sql .= "email='" . db_escape($db, $user['email']) . "', ";
+    if($password_sent) {
+        $sql .= "hashed_password='" . db_escape($db, $hashed_password) . "', ";
+    }
+    $sql .= "username='" . db_escape($db, $user['username']) . "' ";
+    $sql .= "WHERE id='" . db_escape($db, $user['id']) . "' ";
+    $sql .= "LIMIT 1";
+    $result = mysqli_query($db, $sql);
+    if($result) {
+        return true;
+    } else {
+        echo mysqli_error($db);
+        db_disconnect($db);
+        exit;
+    }
+}
 
-function delete_user($admin) {}
+function delete_user($user) {}
     
 ?>
