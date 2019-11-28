f4b82dbedf391edf63ca0b6f2725d0882e863fbc
David Spencer <david.spencer@kcl.ac.uk>
2019-11-28 10:06:16 +0000

    add user input validation for profile updates

--- private/query_functions.php
+++ private/query_functions.php
@@ -13,7 +13,43 @@
     }
 
     function validate_user($user, $options=[]) {
-        // TODO: implement complete user validation
+        if(is_blank($user['first_name'])) {
+            $errors[] = "Please enter your first name.";
+        } elseif (!has_length($user['first_name'], array('min' => 2, 'max' => 255))) {
+            $errors[] = "Please enter a valid first name.";
+        }
+        if(is_blank($user['last_name'])) {
+            $errors[] = "Please enter your last name.";
+        } elseif (!has_length($user['last_name'], array('min' => 2, 'max' => 255))) {
+            $errors[] = "Please enter a valid last name.";
+        }
+        if(is_blank($user['dob'])) {
+            $errors[] = "Please enter your date of birth.";
+        } elseif (!has_length($user['dob'], array('min' => 7, 'max' => 10))) {
+            $errors[] = "Please enter your date of birth as YYYY-MM-DD.";
+        }
+        if(is_blank($user['gender'])) {
+            $errors[] = "Please enter your gender M/F/O.";
+        } elseif (!has_length($user['gender'], array('min' => 1, 'max' => 1))) {
+            $errors[] = "Please enter your gender as M or F or O.";
+        }
+        if(is_blank($user['phone'])) {
+            $errors[] = "Please enter your phone number.";
+        } elseif (!has_length($user['phone'], array('min' => 9, 'max' => 11))) {
+            $errors[] = "Please enter a valid phone number.";
+        }
+        if(is_blank($user['address'])) {
+            $errors[] = "Please enter your mailing address.";
+        } elseif (!has_length($user['address'], array('min' => 10, 'max' => 255))) {
+            $errors[] = "Please enter a valid mailing address.";
+        }
+        if(is_blank($user['email'])) {
+            $errors[] = "Please enter your email address.";
+        } elseif (!has_length($user['email'], array('max' => 255))) {
+            $errors[] = "Email address must be less than 255 characters.";
+        } elseif (!has_valid_email_format($user['email'])) {
+            $errors[] = "Please enter a valid email address.";
+        }
         $password_required = $options['password_required'] ?? true;
         if($password_required) {
             if(is_blank(user['password'])) {
