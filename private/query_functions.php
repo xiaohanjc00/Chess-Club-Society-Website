355e7d79877467d3c300a7e60b4bcec8adeee2b5
k1895709 <qkieran53@hotmail.com>
2019-12-09 00:37:26 +0000

    update database functions

--- private/query_functions.php
+++ private/query_functions.php
@@ -295,7 +295,115 @@ function update_article_image($link, $id) {
     }
   }
 
-  function find_all_events() {
+//  function find_all_events() {
+//    global $db;
+//
+//    $sql = "SELECT * FROM opening_event ORDER BY eventDate DESC";
+//    $result = mysqli_query($db, $sql);
+//    confirm_result_set($result);
+//    return $result;
+//  }
+//
+//  function find_event_by_id($id) {
+//    global $db;
+//
+//    $sql = "SELECT * FROM opening_event WHERE eventID =" . $id;
+//    $result = mysqli_query($db, $sql);
+//    confirm_result_set($result);
+//    return $result;
+//  }
+//
+//  function delete_event($id) {
+//    global $db;
+//
+//    $sql = "DELETE FROM opening_event WHERE eventID= ".$id;
+//    $result = mysqli_query($db, $sql);
+//    if($result) {
+//      return true;
+//    } else {
+//      // DELETE failed
+//      echo mysqli_error($db);
+//      db_disconnect($db);
+//      exit;
+//    }
+//  }
+//
+//  function insert_event($title, $description) {
+//    global $db;
+//
+//    $sql = 'INSERT INTO opening_event(eventTitle, eventDesc, eventDate) values ("' . $title . '","' . $description . '", now());';
+//    $result = mysqli_query($db, $sql);
+//    if($result) {
+//      return true;
+//    } else {
+//      // DELETE failed
+//      echo mysqli_error($db);
+//      db_disconnect($db);
+//      exit;
+//    }
+//  }
+//
+//  function insert_event_image($title, $description, $link) {
+//    global $db;
+//
+//    $sql = 'INSERT INTO opening_event(eventTitle, eventDesc, eventDate, eventImage) values ("' . $title . '","' . $description . '", now());';
+//    $result = mysqli_query($db, $sql);
+//    if($result) {
+//      return true;
+//    } else {
+//      // DELETE failed
+//      echo mysqli_error($db);
+//      db_disconnect($db);
+//      exit;
+//    }
+//  }
+//
+//function update_event_image($link, $id) {
+//    global $db;
+//
+//    $sql = 'UPDATE opening_event set eventImage="'. $link .'" WHERE eventID = ' .$id.';';
+//    $result = mysqli_query($db, $sql);
+//    if($result) {
+//      return true;
+//    } else {
+//      // DELETE failed
+//      echo mysqli_error($db);
+//      db_disconnect($db);
+//      exit;
+//    }
+//  }
+//
+//  function update_event_title($title, $id) {
+//    global $db;
+//
+//    $sql = 'UPDATE opening_event set eventTitle= "'. $title .'" WHERE eventID =' .$id.';';
+//    $result = mysqli_query($db, $sql);
+//    if($result) {
+//      return true;
+//    } else {
+//      // DELETE failed
+//      echo mysqli_error($db);
+//      db_disconnect($db);
+//      exit;
+//    }
+//  }
+//
+//  function update_event_description($description, $id) {
+//    global $db;
+//
+//    $sql = 'UPDATE opening_event set eventDescription="'. $description .'" WHERE eventID = ' .$id.';';
+//    $result = mysqli_query($db, $sql);
+//    if($result) {
+//      return true;
+//    } else {
+//      // DELETE failed
+//      echo mysqli_error($db);
+//      db_disconnect($db);
+//      exit;
+//    }
+//  } 
+
+ function find_all_events() {
     global $db;
 
     $sql = "SELECT * FROM opening_event ORDER BY eventDate DESC";
@@ -376,7 +484,7 @@ function update_event_image($link, $id) {
   function update_event_title($title, $id) {
     global $db;
 
-    $sql = 'UPDATE opening_event set eventTitle= "'. $title .'" WHERE eventID =' .$id.';';
+    $sql = 'UPDATE opening_event set articleTitle= "'. $title .'" WHERE articleID =' .$id.';';
     $result = mysqli_query($db, $sql);
     if($result) {
       return true;
@@ -401,7 +509,7 @@ function update_event_image($link, $id) {
       db_disconnect($db);
       exit;
     }
-  } 
+  }
      
     function find_user_by_id($id) {
         global $db;
