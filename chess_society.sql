4bf0d05acb9e5e0a38799d7a94a733c1fb2b0f79
David Spencer <david.spencer@kcl.ac.uk>
2019-12-11 14:52:15 +0000

    Remove unused folder and update SQL

--- chess_society.sql
+++ chess_society.sql
@@ -39,12 +39,12 @@ CREATE TABLE `users` (
 ALTER TABLE users ADD INDEX index_username (username);
 
 -- add some test data to users table
-INSERT INTO users(admin, first_name, last_name, dob, gender, phone, address, email, username, hashed_password) VALUES
-    (1, 'Joe','Baker','1998-03-30','M',020862210922,'20 Richmond Avenue, Croydon CR46YW','jo@bakercake.com','joebaker','secret'),
-    (0, 'Jane','Xu','1978-03-10','F',020867770922,'21 Richmond Avenue, London CWC2R4EW','up@down.com','chessy','password'),
-    (0, 'Jenny','Marston','1938-05-20','F',020862287622,'20 Violet Street, Mitcham MH46YW','jjjj_y@last.com','chesser','word'),
-    (0, 'Jude','Stoyanov','1999-04-22','M',020845610922,'20 West Street, Barnet N66YW','stoyupol@aol.com','JudeKnight','pass'),
-    (1, 'Adam','Stoyanov','1999-04-22','M',020845610922,'20 West Street, Barnet N66YW','stoyupol@aol.com','JudeKnight','pass');
+INSERT INTO users(admin, first_name, last_name, dob, gender, phone, address, email, username) VALUES
+    (1, 'Joe','Baker','1998-03-30','M',020862210922,'20 Richmond Avenue, Croydon CR46YW','jo@bakercake.com','joebaker'),
+    (0, 'Jane','Xu','1978-03-10','F',020867770922,'21 Richmond Avenue, London CWC2R4EW','up@down.com','chessy'),
+    (0, 'Jenny','Marston','1938-05-20','F',020862287622,'20 Violet Street, Mitcham MH46YW','jjjj_y@last.com','chesser'),
+    (0, 'Jude','Stoyanov','1999-04-22','M',020845610922,'20 West Street, Barnet N66YW','stoyupol@aol.com','JudeKnight'),
+    (1, 'Adam','Stoyanov','1999-08-22','M',020845610922,'20 West Street, Barnet N66YW','stoyupolo@aol.com','JudeKnight');
 
 
 
