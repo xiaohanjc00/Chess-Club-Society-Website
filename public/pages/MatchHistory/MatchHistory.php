4d4ac2d60dc9b796ea513eb9c940863eb4d323d0
k1889687 <xiaohan.jiang_chen@kcl.ac.uk>
2019-12-14 09:31:52 +0000

    MatchHistory code cleaning

--- public/pages/MatchHistory/MatchHistory.php
+++ public/pages/MatchHistory/MatchHistory.php
@@ -3,12 +3,9 @@
 <?php
     if(!isset($_GET['id'])) {
         redirect_to(url_for('../profile/index.php'));
-      }
-      $id = $_GET['id'];
-      
-?>
-
-<?php
+    }
+    
+    $id = $_GET['id'];   
     $matches = find_all_matches($id);
 ?>
 
@@ -17,6 +14,7 @@
     <!--Main container-->
     <div>
         <table table class="table table-hover table-dark">
+
             <thead>
                 <tr>
                 <th scope="col">Tournament Name</th>
@@ -27,26 +25,29 @@
                 <th scope="col">New ELO rating</th>
                 </tr>
             </thead>
-        <tbody>
-            <?php
 
-                while($row = mysqli_fetch_assoc($matches)){
-                    echo "<tr>";
-                    echo "<th scope='col'>". $row["tournamentName"]. "</th>";
-                    echo "<th scope='col'>". $row["tournamentDate"]. "</th>";
-                    echo "<th scope='col'>". $row["roundNumber"]. "</th>";
-                    $name = (get_member_name($row["roundWinner"])->fetch_assoc());
-                    echo "<th scope='col'>". $name["first_name"]. "</th>";
-                    $rating = get_elo($id,  $row["tournamentID"]);
-                    echo "<th scope='col'>". $rating['before']. "</th>";
-                    echo "<th scope='col'>". $rating['after']. "</th>";
-                    echo "</tr>";
-                }
+            <tbody>
+                <?php
 
-            ?>
+                    while($row = mysqli_fetch_assoc($matches)){
+                        echo "<tr>";
+                        echo "<th scope='col'>". $row["tournamentName"]. "</th>";
+                        echo "<th scope='col'>". $row["tournamentDate"]. "</th>";
+                        echo "<th scope='col'>". $row["roundNumber"]. "</th>";
+
+                        $name = (get_member_name($row["roundWinner"])->fetch_assoc());
+                        echo "<th scope='col'>". $name["first_name"]. "</th>";
+                        
+                        $rating = get_elo($id,  $row["tournamentID"]);
+                        echo "<th scope='col'>". $rating['before']. "</th>";
+                        echo "<th scope='col'>". $rating['after']. "</th>";
+                        echo "</tr>";
+                    }
+
+                ?>
 
             </tbody>
         </table>
     </div>
 
-    <?php include(SHARED_PATH . '/footer.php'); ?>
+<?php include(SHARED_PATH . '/footer.php'); ?>
