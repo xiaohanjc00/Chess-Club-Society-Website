fbb43518561c860295aed39aeea2c0d26eaed57c
k1889687 <xiaohan.jiang_chen@kcl.ac.uk>
2019-12-14 09:10:43 +0000

    Tournament code cleaning

--- public/pages/Tournament/edit.php
+++ public/pages/Tournament/edit.php
@@ -1,46 +1,46 @@
 <?php require_once(realpath(dirname(__FILE__) . '/../../..'). '/private/initialise.php'); ?>
 <?php include(SHARED_PATH . '/header.php'); ?>
 
-    <?php
-        if(is_post_request()) {
-            $tournament = [];
-            $tournament['organizer'] = $_POST['organizer'] ?? '';
-            $tournament['name'] = $_POST['name'] ?? '';
-            $tournament['date'] = $_POST['date'] ?? '';
-            $tournament['deadline'] = $_POST['deadline'] ?? '';
-            $id = isset($_GET['id']) ? $_GET['id'] : '';
-            $result = update_tournament($tournament, $id);
-            if($result === true) {
-                $_SESSION['message'] = 'New tournament successfully created!';
-                echo '<meta http-equiv="refresh" content="0;URL=index.php"/>';
-            } else {
-                $errors = $result;
-            }
+<?php
+    if(is_post_request()) {
+        $tournament = [];
+        $tournament['organizer'] = $_POST['organizer'] ?? '';
+        $tournament['name'] = $_POST['name'] ?? '';
+        $tournament['date'] = $_POST['date'] ?? '';
+        $tournament['deadline'] = $_POST['deadline'] ?? '';
+        $id = isset($_GET['id']) ? $_GET['id'] : '';
+        $result = update_tournament($tournament, $id);
+        if($result === true) {
+            $_SESSION['message'] = 'New tournament successfully created!';
+            echo '<meta http-equiv="refresh" content="0;URL=index.php"/>';
         } else {
-            $tournament = [];
-            $tournament['organizer'] = '';
-            $tournament['name'] = '';
-            $tournament['date'] = '';
-            $tournament['deadline'] = '';
+            $errors = $result;
         }
-    ?>
+    } else {
+        $tournament = [];
+        $tournament['organizer'] = '';
+        $tournament['name'] = '';
+        $tournament['date'] = '';
+        $tournament['deadline'] = '';
+    }
+?>
 
-    <?php
-        echo '<div >' ;
-        echo '<h4>Edit a tournament now!</h4>';
-        echo '<p>Please fill in the forms:</p>';  
+<?php
+    echo '<div >' ;
+    echo '<h4>Edit a tournament now!</h4>';
+    echo '<p>Please fill in the forms:</p>';  
 
-        echo display_errors($errors);
+    echo display_errors($errors);
 
-        echo '<form width="800px" margin="auto"  action="edit.php?id='. $_GET['id'] .'" method="post">';
-        echo '<dl> <dt>Tournament organizer:</dt><dd><input type="text" name="organizer" value="'.$tournament['organizer'] . '" /></dd> </dl>' ;
-        echo '<dl> <dt>Tournament Name:</dt><dd><input type="text" name="name" value="'.$tournament['name'] . '" /></dd> </dl>' ;
-        echo '<dl> <dt>Tournament date:</dt><dd><input type="date" name="date" value="'.$tournament['date'] . '" /></dd> </dl>' ;
-        echo '<dl> <dt>Registration deadline:</dt><dd><input type="date" name="deadline" value="'.$tournament['deadline'] . '" /></dd> </dl>' ;
+    echo '<form width="800px" margin="auto"  action="edit.php?id='. $_GET['id'] .'" method="post">';
+    echo '<dl> <dt>Tournament organizer:</dt><dd><input type="text" name="organizer" value="'.$tournament['organizer'] . '" /></dd> </dl>' ;
+    echo '<dl> <dt>Tournament Name:</dt><dd><input type="text" name="name" value="'.$tournament['name'] . '" /></dd> </dl>' ;
+    echo '<dl> <dt>Tournament date:</dt><dd><input type="date" name="date" value="'.$tournament['date'] . '" /></dd> </dl>' ;
+    echo '<dl> <dt>Registration deadline:</dt><dd><input type="date" name="deadline" value="'.$tournament['deadline'] . '" /></dd> </dl>' ;
 
-        echo '<div> <input type="submit" value="Post new tournament" /> </div>';
-        echo '</form>';
-        echo '</div>';
-    ?>
+    echo '<div> <input type="submit" value="Post new tournament" /> </div>';
+    echo '</form>';
+    echo '</div>';
+?>
 
 <?php include(SHARED_PATH . '/footer.php'); ?>
--- public/pages/Tournament/new.php
+++ public/pages/Tournament/new.php
@@ -1,29 +1,33 @@
 <?php require_once(realpath(dirname(__FILE__) . '/../../..'). '/private/initialise.php'); ?>
- <?php include(SHARED_PATH . '/header.php'); ?>
-    <?php
-    if($_GET["add"] == "tournament"){
-            if(is_post_request()) {
-                $id = isset($_GET['id']) ? $_GET['id'] : '';
-                $tournament = [];
-                $tournament['organizer'] = $id;
-                $tournament['name'] = $_POST['name'] ?? '';
-                $tournament['deadline'] = $_POST['deadline'] ?? '';
-                $tournament['date'] = $_POST['date'] ?? '';
+<?php include(SHARED_PATH . '/header.php'); ?>
+<?php
 
-                $result = insert_tournament($tournament);
-                if($result === true) {
-                    $_SESSION['message'] = 'New tournament successfully created!';
-                    echo '<meta http-equiv="refresh" content="0;URL=index.php"/>';
-                } else {
-                    $errors = $result;
-                }
-            } else {
-                $tournament = [];
-                $tournament['organizer'] = '';
-                $tournament['name'] = '';
-                $tournament['deadline'] = '';
-                $tournament['date'] = '';
+    if($_GET["add"] == "tournament"){
+        if(is_post_request()) {
+            $id = isset($_GET['id']) ? $_GET['id'] : '';
+            $tournament = [];
+            $tournament['organizer'] = $id;
+            $tournament['name'] = $_POST['name'] ?? '';
+            $tournament['deadline'] = $_POST['deadline'] ?? '';
+            $tournament['date'] = $_POST['date'] ?? '';
+
+            $result = insert_tournament($tournament);
+
+            if($result === true) {
+                $_SESSION['message'] = 'New tournament successfully created!';
+                echo '<meta http-equiv="refresh" content="0;URL=index.php"/>';
+            } 
+            else {
+                $errors = $result;
             }
+        } 
+        else {
+            $tournament = [];
+            $tournament['organizer'] = '';
+            $tournament['name'] = '';
+            $tournament['deadline'] = '';
+            $tournament['date'] = '';
+        }
 
 
         echo "<link rel='stylesheet' href='../stylesheets/newsStyle.css'>";
@@ -32,45 +36,51 @@
         echo "<h2>Tournament</h2>";
         echo "</div>";
 
-            echo '<div >' ;
-            echo '<h4>Create a new tournament!<h4>';
-            echo '<p>Please fill in the forms:</p>';  
+        echo '<div >' ;
+        echo '<h4>Create a new tournament!<h4>';
+        echo '<p>Please fill in the forms:</p>';  
 
-           echo display_errors($errors);
-            echo '<form width="800px" margin="auto"  action="new.php?add='. $_GET['add'] .'&&id='. $_GET['id'] . '" method="post">';
-            echo '<dl> <dt>Tournament Name:</dt><dd><input type="text" name="name" value="'. $tournament['name'] . '" /></dd> </dl>' ;
-            echo '<dl> <dt>Tournament date:</dt><dd><input type="date" name="date" value="'. $tournament['date'] . '" /></dd> </dl>' ;
-            echo '<dl> <dt>Registration deadline:</dt><dd><input type="date" name="deadline" value="'. $tournament['deadline'] . '" /></dd> </dl>' ;
+        echo display_errors($errors);
 
-            echo '<div> <input type="submit" value="Post new tournament" /> </div>';
-            echo '</form>';
-            echo '</div>';
+        echo '<form width="800px" margin="auto"  action="new.php?add='. $_GET['add'] .'&&id='. $_GET['id'] . '" method="post">';
+        echo '<dl> <dt>Tournament Name:</dt><dd><input type="text" name="name" value="'. $tournament['name'] . '" /></dd> </dl>' ;
+        echo '<dl> <dt>Tournament date:</dt><dd><input type="date" name="date" value="'. $tournament['date'] . '" /></dd> </dl>' ;
+        echo '<dl> <dt>Registration deadline:</dt><dd><input type="date" name="deadline" value="'. $tournament['deadline'] . '" /></dd> </dl>' ;
+
+        echo '<div> <input type="submit" value="Post new tournament" /> </div>';
+        echo '</form>';
+        echo '</div>';
     }
-    else if($_GET["add"] == "coorganizer"){
-            if(is_post_request()) {
-                $id = isset($_GET['id']) ? $_GET['id'] : '';
-                $tournament = [];
-                $tournament['coorganizer'] = $_POST['coorganizer'] ?? '';
-                $tournament['tournamentID'] = $id ;
 
-                $result = insert_tournament_organizer($tournament);
-                if($result === true) {
-                    $_SESSION['message'] = 'New co-organizer successfully added!';
-                    echo $_SESSION['message'];
-                    echo '<meta http-equiv="refresh" content="0;URL=index.php"/>';
-                } else {
-                    $_SESSION['Error'] = "All admins have been assigned to this tournament !";
-                    if (isset($_SESSION["Error"])) { 
-                        echo  $_SESSION["Error"] ; 
-                        unset($_SESSION["Error"]); 
-                     }
-                    echo '<meta http-equiv="refresh" content="0;URL=index.php"/>';
+    else if($_GET["add"] == "coorganizer"){
+        if(is_post_request()) {
+            $id = isset($_GET['id']) ? $_GET['id'] : '';
+            $tournament = [];
+            $tournament['coorganizer'] = $_POST['coorganizer'] ?? '';
+            $tournament['tournamentID'] = $id ;
+
+            $result = insert_tournament_organizer($tournament);
+
+            if($result === true) {
+                $_SESSION['message'] = 'New co-organizer successfully added!';
+                echo $_SESSION['message'];
+                echo '<meta http-equiv="refresh" content="0;URL=index.php"/>';
+            } 
+            else {
+                $_SESSION['Error'] = "All admins have been assigned to this tournament !";
+
+                if (isset($_SESSION["Error"])){ 
+                    echo  $_SESSION["Error"] ; 
+                    unset($_SESSION["Error"]); 
                 }
-            } else {
-                $tournament = [];
-                $tournament['coorganizer'] = '';
-                $tournament['tournamentID'] = '';
+                echo '<meta http-equiv="refresh" content="0;URL=index.php"/>';
             }
+        } 
+        else {
+            $tournament = [];
+            $tournament['coorganizer'] = '';
+            $tournament['tournamentID'] = '';
+        }
         
         echo "<link rel='stylesheet' href='../stylesheets/newsStyle.css'>" ;
 
@@ -88,6 +98,7 @@
         $admins = find_admins($_GET['id']);
 
         echo "<select name='coorganizer'>";
+
         if (mysqli_num_rows($admins) > 0) {
             while($row = mysqli_fetch_assoc($admins)){
                 echo "<option value=" . $row["id"] .  ">".  $row["first_name"] ." ". $row["last_name"] . "</option>";
@@ -101,24 +112,30 @@
         echo '</form>';
         echo '</div>';
     }
+
     else if($_GET["add"] == "participant"){
         insert_tournament_participant($_GET["userid"], $_GET["id"]);
         echo '<meta http-equiv="refresh" content="0;URL=index.php"/>';     
     }
+
     else if($_GET["add"] == "match"){
 
         function scheduler($teams){
             $round = [];
+
             if (count($teams)%2 != 0){
                 array_push($teams,"not playing");
             }
+
             $away = array_splice($teams,(count($teams)/2));
             $home = $teams;
+
             for ($i=0; $i < count($home)+count($away)-1; $i++){
                 for ($j=0; $j<count($home); $j++){
                     $round[$i][$j]["Home"]=$home[$j];
                     $round[$i][$j]["Away"]=$away[$j];
                 }
+
                 if(count($home)+count($away)-1 > 2){
                     $splice = array_splice($home,1,1);
                     array_unshift($away, array_shift($splice));
@@ -158,12 +175,12 @@
             $schedule = scheduler($members);
             get_match($schedule, 0);
         }
+
         generate_schedule();
         echo '<meta http-equiv="refresh" content="0;URL=index.php"/>';
             
     }
 
-    ?>
-   
+?>
   
-  <?php include(SHARED_PATH . '/footer.php'); ?>
+<?php include(SHARED_PATH . '/footer.php'); ?>
